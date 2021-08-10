4eba53a559fd8e994f0ad4354ab1eac530ea7c1e
Olivier <orefalo@gmail.com>
2021-08-10 17:51:35 +0300

    ch

--- .vuepress/config.ts
+++ .vuepress/config.ts
@@ -1,96 +1,93 @@
-import {defineUserConfig} from '@vuepress/cli'
-import type {DefaultThemeOptions} from '@vuepress/theme-default'
-import {path} from '@vuepress/utils'
-import {navbar, sidebar} from './configs'
+import { defineUserConfig } from '@vuepress/cli'
+import type { DefaultThemeOptions } from '@vuepress/theme-default'
+import { path } from '@vuepress/utils'
+import { navbar, sidebar } from './configs'
 
 const isProd = process.env.NODE_ENV === 'production'
 
 export default defineUserConfig<DefaultThemeOptions>({
-    base: '/mu/documentation/',
-    port: 8080,
-    host: "127.0.0.1",
-    head: [["link", {rel: "icon", href: "/favicon.ico"}]],
-    bundler: '@vuepress/webpack',
-    locales: {
-        '/': {
-            lang: 'en-US',
-            title: 'Mu Documentation',
-            description: 'Serverless functions in a box',
-        },
+  base: '/mu/documentation/',
+  port: 8080,
+  host: '127.0.0.1',
+  head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
+  bundler: '@vuepress/webpack',
+  locales: {
+    '/': {
+      lang: 'en-US',
+      title: 'Mu Documentation',
+      description: 'Serverless functions in a box',
     },
-    // theme: path.resolve(__dirname, './theme'),
-    markdown: {
-        importCode: {
-            handleImportPath: (str) =>
-                str.replace(
-                    /^@vuepress/,
-                    path.resolve(__dirname, '../../packages/@vuepress')
-                ),
-        },
+  },
+  // theme: path.resolve(__dirname, './theme'),
+  markdown: {
+    importCode: {
+      handleImportPath: (str) =>
+        str.replace(
+          /^@vuepress/,
+          path.resolve(__dirname, '../../packages/@vuepress')
+        ),
     },
-    plugins: [
-        // ['@vuepress/search', {searchMaxSuggestions: 10}],
-        // ['@vuepress/plugin-search',
-        //   {
-        //     isSearchable: (page) => page.path !== '/',
-        //     locales: {
-        //       '/': {
-        //         placeholder: 'Search',
-        //       },
-        //       '/zh/': {
-        //         placeholder: '搜索',
-        //       },
-        //     },
-        //   }],
-        // ['fulltext-search'],      "vuepress-plugin-fulltext-search": "^2.2.1",
-        // ['@vuepress/theme-default'],
-        // ['@vuepress/back-to-top'],
-        ['@vuepress/medium-zoom', {
-            selector: 'img'
-        }],
-
-        // ['@vuepress/plugin-google-analytics', {id: "UA-124241826-1",}],
-        // ['@vuepress/plugin-pwa'],
-        // ['@vuepress/plugin-register-components',
-        //     {
-        //         componentsDir: path.resolve(__dirname, './components'),
-        //     }],
-        // only enable shiki plugin in production mode
-        // [
-        //     '@vuepress/plugin-shiki',
-        //     {
-        //         theme: 'dark-plus',
-        //     }
-        // ],
+  },
+  plugins: [
+    [
+      '@vuepress/plugin-search',
+      {
+        isSearchable: (page) => page.path !== '/',
+        locales: {
+          '/': {
+            placeholder: 'Search',
+          },
+        },
+      },
+    ],
+    [
+      '@vuepress/medium-zoom',
+      {
+        selector: 'img',
+      },
     ],
 
-    themeConfig: {
-        // extends: '@vuepress/theme-default',
-        // layouts: path.resolve(__dirname, 'theme/layouts'),
-        // fulltext-search plugin
-        // searchMaxSuggestions: 10,
-        logo: "/images/logo.svg",
-        repo: 'lean-mu/mu-helm',
-        //docsDir: "docs",
+    ['@vuepress/plugin-google-analytics', { id: 'UA-124241826-1' }],
+    // ['@vuepress/plugin-register-components',
+    //     {
+    //         componentsDir: path.resolve(__dirname, './components'),
+    //     }],
+    // only enable shiki plugin in production mode
+    [
+      '@vuepress/plugin-shiki',
+      {
+        theme: 'dark-plus',
+      },
+    ],
+  ],
 
-        // theme-level locales config
-        locales: {
-            /**
-             * English locale config
-             *
-             * As the default locale of @vuepress/theme-default is English,
-             * we don't need to set all of the locale fields
-             */
-            '/': {
-                // navbar
-                navbar: navbar,
+  themeConfig: {
+    // extends: '@vuepress/theme-default',
+    // layouts: path.resolve(__dirname, 'theme/layouts'),
+    // fulltext-search plugin
+    // searchMaxSuggestions: 10,
+    logo: '/images/logo.svg',
+    repo: 'lean-mu/mu-helm',
+    //docsDir: "docs",
 
-                // sidebar
-                sidebar: sidebar,
+    // theme-level locales config
+    locales: {
+      /**
+       * English locale config
+       *
+       * As the default locale of @vuepress/theme-default is English,
+       * we don't need to set all of the locale fields
+       */
+      '/': {
+        // navbar
+        navbar: navbar,
 
-                // page meta
-                editLinkText: 'Edit this page on GitHub',
-            },
-        }
-    }
+        // sidebar
+        sidebar: sidebar,
+
+        // page meta
+        editLinkText: 'Edit this page on GitHub',
+      },
+    },
+  },
 })
--- deepdive/accessFS.md
+++ deepdive/accessFS.md
@@ -1,13 +1,12 @@
-# Accessing File Systems from Running Functions
+# Accessing the File Systems at Runtime
 
-A function you've deployed can access the file system of the container in which it's running as follows:
+A function can access the file system of the container in which it is running as follows:
 
-- the function can read files from all directories
-- the function can write files to the /tmp directory
+- the function can **read** files from all directories
+- the function can **write** files **only** to the /tmp directory
 
 For example, you might want a function to download an Excel file and then read its contents.
-To meet this requirement, you might create a function that writes the file to the /tmp directory in the
-container's filesystem, and then subsequently reads the file.
+To meet this requirement, you might create a function that writes the file to the /tmp directory in the container's filesystem, and then subsequently reads the file.
 
 When writing files to the /tmp directory, the /tmp  directory is generally always writable.
 However, the maximum allowable size of the /tmp directory depends on the maximum memory threshold
@@ -22,4 +21,4 @@ specified for the function:
 
 Note that the /tmp directory might be shared by successive invocations of the function.
 A file written by an earlier invocation of a function could still exist when the function is invoked a second time.
-It is the developer responsibility to manage the lifecycle of these files to avoid unexpected behavior.
+It is the developer responsibility to manage the lifecycle of these files to avoid unexpected behaviors.
